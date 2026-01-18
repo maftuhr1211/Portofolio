f47de9b50f472c18df467fbc5d44fe157c349cdf
maftuhr1211 <maftuhr1211@gmail.com>
2026-01-18 16:35:01 +0700

    porto

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',   // ⬅️ TAMBAHKAN INI
+  base: '/Portofolio/',   // ⬅️ TAMBAHKAN INI
   plugins: [react(), tailwindcss()],
 })
