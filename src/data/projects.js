e24515040c59eced5abebefcabb9cf2624f88eed
maftuhr1211 <maftuhr1211@gmail.com>
2026-03-31 14:06:17 +0700

    porto

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,6 +10,14 @@ import p9 from '../assets/projects/p9.png';
 import p10 from '../assets/projects/p10.png';
 import p11 from '../assets/projects/p11.png';
 import p12 from '../assets/projects/p12.png';
+import p13 from '../assets/projects/p13.png';
+import p14 from '../assets/projects/p14.png';
+import p15 from '../assets/projects/p15.png';
+import p16 from '../assets/projects/p16.png';
+import p17 from '../assets/projects/p17.png';
+import p18 from '../assets/projects/p18.png';
+import p19 from '../assets/projects/p19.png';
+import p20 from '../assets/projects/p20.png';
 
 export const projects = [
   {
@@ -76,7 +84,7 @@ export const projects = [
     id: 7,
     title: 'Experimental Design',
     description: 'xxx',
-    images: [p1, p12,],
+    images: [p13, p14,],
     category: 'Other',
     technologies: ['Figma', 'Prototype'],
     metrics: 'Experimental visual exploration',
@@ -86,10 +94,30 @@ export const projects = [
     id: 8,
     title: 'Experimental Design',
     description: 'xxx',
-    images: [p8, p1, p2],
+    images: [p15, p16],
     category: 'Other',
     technologies: ['Figma', 'Prototype'],
     metrics: 'Experimental visual exploration',
+    demoUrl: 'xxx',
+  },
+  {
+    id: 9,
+    title: 'Poster Edukasi',
+    description: 'xxx',
+    images: [p17, p18],
+    category: 'Poster',
+    technologies: ['Figma', 'Design'],
+    metrics: 'Visual communication & layout exploration',
+    demoUrl: 'xxx',
+  },
+  {
+    id: 10,
+    title: 'Poster Edukasi',
+    description: 'xxx',
+    images: [p19, p20],
+    category: 'Poster',
+    technologies: ['Figma', 'Design'],
+    metrics: 'Visual communication & layout exploration',
     demoUrl: 'xxx',
   },
 ];
